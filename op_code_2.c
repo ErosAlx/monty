f5ae0176bd1f42ec3485426288ce1a7aad74716d
ErosAlx <erosalx1@gmail.com>
2023-12-14 11:06:49 +0100

    Stacks, Queues

--- op_code_2.c
+++ op_code_2.c
@@ -1,18 +1,18 @@
 #include "monty.h"
 /**
  * sub_opcode - subtracts the top element of the stack
- * from the second top element of the stack.
+ * from the second top element of the stack
  * @stack: Pointer to a pointer pointing to top node of the stack
- * @line_number: Interger representing the line number of of the opcode
+ * @line_number: interger representing the line number of of the opcode
  */
 
 void sub_opcode(stack_t **stack, unsigned int line_number)
 {
 
-        if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-                error_func2(8, line_number, "sub");
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		error_func2(8, line_number, "sub");
 
-        (*stack)->next->n -= (*stack)->n;
+	(*stack)->next->n = (*stack)->next->n - (*stack)->n;
 	*stack = (*stack)->next;
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
@@ -20,21 +20,21 @@ void sub_opcode(stack_t **stack, unsigned int line_number)
 
 /**
  * div_opcode - divides the second top element of the stack
- * by the top element of the stack.
+ * by the top element of the stack
  * @stack: Pointer to a pointer pointing to top node of the stack
- * @line_number: Interger representing the line number of of the opcode
+ * @line_number: interger representing the line number of of the opcode
  */
 
 void div_opcode(stack_t **stack, unsigned int line_number)
 {
 
-        if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-                error_func2(8, line_number, "div");
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		error_func2(8, line_number, "div");
 	if ((*stack)->n == 0)
-                error_func2(9, line_number, "div");
+		error_func2(9, line_number, "div");
 
-        (*stack)->next->n /= (*stack)->n;
-        *stack = (*stack)->next;
-        free((*stack)->prev);
-        (*stack)->prev = NULL;
+	(*stack)->next->n = (*stack)->next->n / (*stack)->n;
+	*stack = (*stack)->next;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
 }
